e3602cc895252a9e49172152c697ac56d594a637
m-dennisgocong <m.dennisgocong@gmail.com>
2023-10-18 18:26:40 +0800

    feat: add Navbar and Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const poppins = Poppins({ 
   subsets: ['latin'],
@@ -20,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
+          <Navbar/>
           {children}
+          <Footer/>
       </body>
     </html>
   )
