e6eba50647bd260cbe88c69166a082afbf391c45
m-dennisgocong <m.dennisgocong@gmail.com>
2023-10-18 18:24:37 +0800

    feat: add state management useCartStore
    use zustand for state management for my cart
    create a cart-store contains:
    - isOpen: check if user opens the cart
    - cart: cart list
    - toogleCart: function to open or close the cart
    - addToCart: function to add items from cart list
    - removeProduct: function to remove item from the cart list

---
+++ store/useCartStore.tsx
@@ -0,0 +1,53 @@
+import {create} from 'zustand'
+import { persist } from 'zustand/middleware'
+import { CartType } from '@/types/cartTypes'
+
+type CartState = {
+    isOpen: boolean
+    cart: CartType[]
+    addToCart: (item: CartType) => void
+    toggleCart: () => void
+    removeProduct: (item: CartType) => void
+}
+
+export const useCartStore = create<CartState>()(
+    persist((set) => ({
+        cart: [],
+        isOpen: false,
+        toggleCart: () => set((state) => ({
+            isOpen: !state.isOpen
+        })),
+        addToCart: (item) => set((state) => {
+            const existingItem = state.cart.find((cartItem) => cartItem.id === item.id)
+            if (existingItem){
+                const updateCart = state.cart.map((cartItem) => {
+                    if(cartItem.id === item.id){
+                        return {...cartItem, quantity: cartItem.quantity! + 1}
+                    }
+                    return cartItem
+                });
+                return {cart: updateCart};
+            }else{
+                return {cart: [...state.cart,{...item, quantity: 1}]};
+            }
+        }),
+        removeProduct: (item) => set((state) => {
+            const existingItem = state.cart.find((cartItem) => cartItem.id === item.id)
+            if (existingItem && existingItem.quantity! > 1){
+                const updateCart = state.cart.map((cartItem) => {
+                    if(cartItem.id === item.id){
+                        return {...cartItem, quantity: cartItem.quantity! - 1}
+                    }
+                    return cartItem
+                })
+                return {cart: updateCart};
+            }else{
+                const filteredCart = state.cart.filter((cartItem) => cartItem.id !== item.id)
+                return {cart: filteredCart}
+            }
+        }),
+
+    }),
+    {name: "cart-store"}
+    )
+);
