e88fb9bb0b6f8bdb65b58bed65f2bd31498594d3
m-dennisgocong <m.dennisgocong@gmail.com>
2023-10-31 10:22:38 +0800

    feat: add checkout button & media add breakpoint

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -10,12 +10,16 @@ const Cart = () => {
     const total = totalPrice(cartStore.cart)
     return (
     <div onClick={() => cartStore.toggleCart()} className="fixed w-full h-screen top-0 left-0 bg-black/25 z-50">
-        <div onClick={(e) => e.stopPropagation()} className="bg-white absolute right-0 top-0 md:w-2/5 w-3/4 h-screen p-12">
-            <>
+        <div onClick={(e) => e.stopPropagation()} className="bg-white text-center md:text-left absolute right-0 top-0 lg:px-10 lg:w-2/6 md:w-3/5 w-3/5 h-screen p-5">
+            {cartStore.onCheckout === 'checkout' && (<button onClick={()=> cartStore.setCheckout('cart')} className="">Back to cart</button>)}
+            {
+            cartStore.onCheckout === 'cart' &&  
+                (
+                <>
                 <button onClick={()=> cartStore.toggleCart()} className="">Back to store</button>
                 
                 {cartStore.cart.map((product) => (
-                    <div className="flex py-4 gap-4 items-center border-b-2 border-b-gray-300 justify-between" key={product.id}>
+                    <div className="flex flex-col md:flex-row py-4 gap-4 items-center border-b-2 border-b-gray-300 justify-between md:justify-center" key={product.id}>
                         <Image src={product.image} width={150} height={150} alt="moonlamp"/>
                         <h1 className="font-medium">{product.name}</h1>
                         <div className="flex gap-2 justify-center items-center bg-black/10 px-2 rounded-sm">
@@ -26,13 +30,26 @@ const Cart = () => {
                     </div>
                 ))}
                 {cartStore.cart.length > 0 && <span>Total: {formatPrice(total)}</span>}
-            </>
-            {cartStore.cart.length < 1 && (
-                <div className="h-full flex items-center justify-center">
-                    <span className="text-lg uppercase font-bold whitespace-nowrap">Your cart is empty</span>
-                </div>
-            )}
-            {cartStore.cart.length > 0 && <button className="bg-primary py-2 mt-4 w-full rounded-md text-white">Checkout</button>}
+                </>
+                )
+            }
+            {cartStore.cart.length < 1 && cartStore.onCheckout === 'cart' ? 
+                (
+                    <div className="h-full flex items-center justify-center">
+                        <span className="text-lg uppercase font-bold whitespace-nowrap">Your cart is empty</span>
+                    </div>
+                )
+                : 
+                null
+            }
+            {cartStore.onCheckout === 'cart' && cartStore.cart.length > 0 ? 
+                (<button 
+                    onClick={() => cartStore.setCheckout('checkout')}
+                    className="bg-primary py-2 mt-4 w-full rounded-md text-white">Checkout</button>)
+                :
+                null
+            }
+
         </div>
     </div>
   )
